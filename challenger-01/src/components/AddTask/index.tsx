b8e84586b69ab62b668ef9f17ea1b9657c0193ed
guicoelhodev <gs.coelho@outlook.com>
2022-11-27 14:23:00 -0300

    feat: add button and create a new task based on hook useListActions

--- challenger-01/src/components/AddTask/index.tsx
+++ challenger-01/src/components/AddTask/index.tsx
@@ -3,9 +3,11 @@ import { IoMdAddCircleOutline } from "react-icons/io";
 import { IContextProps, ListContext } from "../../context/ListContext";
 import { DebounceInput } from "react-debounce-input";
 import * as S from "./style";
+import { useListActions } from "../../hooks/useListActions";
 
 export const AddTask: React.FC = () => {
   const { search, setSearch } = useContext(ListContext) as IContextProps;
+  const { addNewTask } = useListActions();
 
   return (
     <S.Container>
@@ -18,7 +20,7 @@ export const AddTask: React.FC = () => {
         }}
         debounceTimeout={500}
       />
-      <button>
+      <button onClick={addNewTask}>
         <p>Criar</p>
         <IoMdAddCircleOutline />
       </button>
