719e0f6b68998b9688101968445a6cb7b338d0ef
guicoelhodev <gs.coelho@outlook.com>
2022-11-27 14:22:33 -0300

    feat: add github anchor to follow guicoelhodev repository

--- challenger-01/src/app/index.tsx
+++ challenger-01/src/app/index.tsx
@@ -1,5 +1,7 @@
 import { Header } from "../components/Header";
+import { Main } from "../components/Main";
 import { ListProvider } from "../context/ListContext";
+import { AiFillGithub } from "react-icons/ai";
 import * as S from "./style";
 
 export const App = () => {
@@ -7,6 +9,14 @@ export const App = () => {
     <S.Container>
       <ListProvider>
         <Header />
+        <Main />
+        <S.Github
+          href="https://github.com/guicoelhodev/ignite_frontend_2022"
+          target="_blank"
+        >
+          <p>guicoelhodev</p>
+          <AiFillGithub />
+        </S.Github>
       </ListProvider>
     </S.Container>
   );
