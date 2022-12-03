db76d6d7e153a7c76ee5117ba2cb0bbd426e9b9c
guicoelhodev <gs.coelho@outlook.com>
2022-12-03 14:33:31 -0300

    feat: create fakeAPI to receive some coffee itens

--- challenger-02/src/app/index.tsx
+++ challenger-02/src/app/index.tsx
@@ -15,3 +15,18 @@ export const App: React.FC = () => {
     </>
   );
 };
+
+// import expressoTradicional from "../assets/coffeeList/Type=Expresso.png";
+// import expressoAmericano from "../assets/coffeeList/Type=Americano.png";
+// import expressoCremoso from "../assets/coffeeList/Type=Expresso Cremoso.png";
+// import expressoGelado from "../assets/coffeeList/Type=Café Gelado.png";
+// import expressoCafeLeite from "../assets/coffeeList/Type=Café com Leite.png";
+// import latte from "../assets/coffeeList/Type=Latte.png";
+// import cappuccino from "../assets/coffeeList/Type=Capuccino.png";
+// import macchiato from "../assets/coffeeList/Type=Macchiato.png";
+// import mochaccino from "../assets/coffeeList/Type=Mochaccino.png";
+// import chocolateQuente from "../assets/coffeeList/Type=Chocolate Quente.png";
+// import cubano from "../assets/coffeeList/Type=Cubano.png";
+// import havaiano from "../assets/coffeeList/Type=Havaiano.png";
+// import arabe from "../assets/coffeeList/Type=Árabe.png";
+// import irlandes from "../assets/coffeeList/Type=Irlandês.png";
