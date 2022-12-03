712934f5b8427aa4a4b672f469f80f4b96f4b6f9
guicoelhodev <gs.coelho@outlook.com>
2022-12-03 13:34:21 -0300

    style: create globalStyle guide

---
+++ challenger-02/src/styles/default/index.ts
@@ -0,0 +1,22 @@
+export const defaultStyleTheme = {
+  "yellow-dark": "#C47F17",
+  "yellow-light": "#F1E9C9",
+  yellow: "#DBAC2C",
+
+  "purple-dark": "#4B2995",
+  "purple-light": "#EBE5F9",
+  purple: "#8047F8",
+
+  "base-title": "#272221",
+  "base-subtitle": "#403937",
+  "base-text": "#574f4d",
+  "base-label": "#8d8686",
+
+  "base-hover": "#d7d5d5",
+  "base-button": "e6e5e5",
+  "base-input": "#ededed",
+  "base-card": "#f3f2f2",
+
+  background: "#fafafa",
+  white: "#ffffff",
+} as const;
