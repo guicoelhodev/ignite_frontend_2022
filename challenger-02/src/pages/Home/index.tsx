3c5a8732ae5f66759e01363c1b4346026882a619
guicoelhodev <gs.coelho@outlook.com>
2022-12-04 22:17:01 -0300

    feat: add Apresentation content

--- challenger-02/src/pages/Home/index.tsx
+++ challenger-02/src/pages/Home/index.tsx
@@ -1,13 +1,80 @@
-import React from "react";
-import { Layout } from "../../components/UI/Layout";
+import React, { useContext } from "react";
 import * as S from "./style";
 
+import HomeImage from "../../assets/img/home.png";
+import { Layout } from "../../components/UI/Layout";
+import { coffeeList as coffeeListData } from "../../data/coffeeList";
+import { CoffeeCard } from "../../components/FC/CoffeeCard";
+import { CoffeeContext, ICoffeeContext } from "../../context/coffeeContext";
+
+import { AiOutlineInbox, AiOutlineShoppingCart } from "react-icons/ai";
+import { BsFillStopwatchFill } from "react-icons/bs";
+import { RiCupLine } from "react-icons/ri";
+import { defaultStyleTheme } from "../../styles/default";
+
+const listIcons = [
+  {
+    text: "Compra simples e segura",
+    icon: <AiOutlineShoppingCart />,
+    bgColor: defaultStyleTheme["yellow-dark"],
+  },
+  {
+    text: "Embalagem mantém o café intacto ",
+    icon: <AiOutlineInbox />,
+    bgColor: defaultStyleTheme["base-text"],
+  },
+  {
+    text: "Entrega rápida e rastreada",
+    icon: <BsFillStopwatchFill />,
+    bgColor: defaultStyleTheme.yellow,
+  },
+  {
+    text: "O café chega fresquinho até você",
+    icon: <RiCupLine />,
+    bgColor: defaultStyleTheme.purple,
+  },
+];
+
 export const Home: React.FC = () => {
+  const { coffeeList } = useContext(CoffeeContext) as ICoffeeContext;
+
   return (
     <Layout>
       <S.Container>
-        <h1>sdihdesuhsduh</h1>
-        <p>that the home page</p>
+        <S.HomeAppresentation>
+          <article>
+            <h1>
+              Encontre o café perfeito <br />
+              para qualquer hora do dia
+            </h1>
+
+            <p>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </p>
+
+            <S.IconList>
+              {listIcons.map((item) => (
+                <S.IconItem bgColor={item.bgColor}>
+                  <span>{item.icon}</span>
+                  <p>{item.text}</p>
+                </S.IconItem>
+              ))}
+            </S.IconList>
+          </article>
+          <img
+            src={HomeImage}
+            alt="imagem que representa um café da Coffee Delivery"
+          />
+        </S.HomeAppresentation>
+        <S.CoffeeList>
+          <h2>Nossos cafés</h2>
+          <ul>
+            {coffeeListData.map((coofeeItem, index) => (
+              <CoffeeCard key={coofeeItem.title + index} {...coofeeItem} />
+            ))}
+          </ul>
+        </S.CoffeeList>
       </S.Container>
     </Layout>
   );
