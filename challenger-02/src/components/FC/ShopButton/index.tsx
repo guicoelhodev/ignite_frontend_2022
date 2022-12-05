448b59de4b2aa89b422f2d700f289f436958696d
guicoelhodev <gs.coelho@outlook.com>
2022-12-04 22:15:35 -0300

    feat: add dinamic coffee information quantity

--- challenger-02/src/components/FC/ShopButton/index.tsx
+++ challenger-02/src/components/FC/ShopButton/index.tsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import * as S from "./style";
 
 import { Button } from "../../UI/Button";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { CoffeeContext, ICoffeeContext } from "../../../context/coffeeContext";
 
 export const ShopButton: React.FC = () => {
+  const { coffeeList } = useContext(CoffeeContext) as ICoffeeContext;
+
   return (
     <S.Container>
       <Button bg="yellow-light" color="yellow-dark">
         <AiOutlineShoppingCart />
       </Button>
 
-      <span>8</span>
+      <span>{coffeeList.length}</span>
     </S.Container>
   );
 };
