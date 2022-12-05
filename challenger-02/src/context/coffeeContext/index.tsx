7974e4a479dae3ad0af351f34323d519d4d42e78
guicoelhodev <gs.coelho@outlook.com>
2022-12-04 22:14:37 -0300

    feat: create coffeeContext to managment all coffee date

---
+++ challenger-02/src/context/coffeeContext/index.tsx
@@ -0,0 +1,36 @@
+import {
+  createContext,
+  Dispatch,
+  DispatchWithoutAction,
+  FC,
+  ReactNode,
+  SetStateAction,
+  useReducer,
+  useState,
+} from "react";
+import { coffeeReducer } from "../../reducers/coffeeReducer/reducer";
+
+export interface ICoffeeObject {
+  name: string;
+  unity_price: number;
+  quantity: number;
+}
+
+export type ICoffeeContext = {
+  coffeeList: ICoffeeObject[] | [];
+  dispatchCoffeeList: any;
+};
+
+export const CoffeeContext = createContext<ICoffeeContext | {}>({});
+
+export const CoffeeProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const [coffeeList, dispatchCoffeeList] = useReducer(coffeeReducer, []);
+
+  console.log("coffeeList", coffeeList);
+
+  return (
+    <CoffeeContext.Provider value={{ coffeeList, dispatchCoffeeList }}>
+      {children}
+    </CoffeeContext.Provider>
+  );
+};
