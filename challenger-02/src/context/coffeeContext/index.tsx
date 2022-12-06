50f8d37afcf6dbc6457e756ef10a26e494fcd8bf
guicoelhodev <gs.coelho@outlook.com>
2022-12-05 22:07:50 -0300

    feat: add new variables to handle modal state

--- challenger-02/src/context/coffeeContext/index.tsx
+++ challenger-02/src/context/coffeeContext/index.tsx
@@ -1,26 +1,46 @@
-import { createContext, FC, ReactNode, useReducer } from "react";
+import {
+  createContext,
+  Dispatch,
+  FC,
+  ReactNode,
+  SetStateAction,
+  useReducer,
+  useState,
+} from "react";
 import { coffeeReducer } from "../../reducers/coffeeReducer/reducer";
 
 export interface ICoffeeObject {
   name: string;
   unity_price: number;
   quantity: number;
+  image: any;
 }
 
 export type ICoffeeContext = {
   coffeeList: ICoffeeObject[] | [];
   dispatchCoffeeList: any;
+
+  showItemsModal: boolean;
+  setShowItemsModal: Dispatch<SetStateAction<boolean>>;
 };
 
-export const CoffeeContext = createContext<ICoffeeContext | {}>({});
+export const CoffeeContext = createContext<ICoffeeContext>(null!);
 
 export const CoffeeProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [coffeeList, dispatchCoffeeList] = useReducer(coffeeReducer, []);
 
+  const [showItemsModal, setShowItemsModal] = useState(false);
   console.log("coffeeList", coffeeList);
 
   return (
-    <CoffeeContext.Provider value={{ coffeeList, dispatchCoffeeList }}>
+    <CoffeeContext.Provider
+      value={{
+        coffeeList,
+        dispatchCoffeeList,
+        showItemsModal,
+        setShowItemsModal,
+      }}
+    >
       {children}
     </CoffeeContext.Provider>
   );
